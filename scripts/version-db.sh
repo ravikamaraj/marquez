efa41945be56fafdf45140fe003cc5a9f54ced94
wslulciuc <willy.lulciuc@wework.com>
2018-12-31 16:51:42 -0800

    Add description for version-db.sh

--- scripts/version-db.sh
+++ scripts/version-db.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+# A utility script for flyway script versioning. The resulting migartion
+# sql script V<timestamp>__<description>.sql will be under the directory
+# src/main/resources/db/migration.
+#
 # Usage: $ ./version-db.sh <description>
 
 set -eu
@@ -8,7 +12,7 @@ readonly PROJECT_ROOT=$(git rev-parse --show-toplevel)
 readonly DB_MIGRATION_PATH="${PROJECT_ROOT}/src/main/resources/db/migration"
 readonly DB_VERSION=$(date +%Y%m%d%H%s)
 
-# Description of versioned migration
+# Description of versioned migration sql script
 description=$(echo "${1}" | xargs)
 
 touch "${DB_MIGRATION_PATH}/V${DB_VERSION}__${description// /_}.sql"
