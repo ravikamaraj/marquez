117d09ed8540d87d5a3c2dcfc55d69ccd19f9654
wslulciuc <willy.lulciuc@wework.com>
2019-01-01 18:53:53 -0800

    Add note on modifying previous migration files

--- scripts/version-db.sh
+++ scripts/version-db.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 # A utility script for versioning database migrations. The resulting migration
-# script V<timestamp>__<description>.sql will be under the directory
-# src/main/resources/db/migration (ex: V20181231221546323003__initial_version.sql)
+# script V<timestamp>__<Description>.sql (ex: V20181231221546323003__Create_owners_table.sql)
+# will be found under the directory src/main/resources/db/migration.
 #
 # Usage: $ ./version-db.sh <description>
+#
+# Note: Do NOT modify previous migration files. The checksum of each file is stored
+#       in the flyway_schema_history table and used to track the state of the
+#       database (see: https://flywaydb.org).
 
 set -eu
 
